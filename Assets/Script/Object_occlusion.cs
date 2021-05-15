d796029698acf3043c44e9c13c7d2ed50ff11174
LH_cloud <1178437028@qq.com>
2021-05-16 00:04:16 +0800

    微调了一下遮挡

--- Assets/Script/Object_occlusion.cs
+++ Assets/Script/Object_occlusion.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[ExecuteInEditMode]
 public class Object_occlusion : MonoBehaviour
 {
-    public GameObject sign;
+
+    public float adjust_h;
 
     // Update is called once per frame
-    void Update()
+#if UNITY_EDITOR
+    void LateUpdate()
     {
+        if(!Application.isPlaying)
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y+ adjust_h);
 
-        transform.position = new Vector3(transform.position.x, transform.position.y, sign.transform.position.y);
+    }
+#endif
 
+    private void OnDrawGizmos()
+    {
+        Vector3 line = new Vector3(transform.position.x, transform.position.y + adjust_h, transform.position.z);
+        Gizmos.DrawLine(line + Vector3.left * 2f, line + Vector3.right * 2f);
     }
 }
