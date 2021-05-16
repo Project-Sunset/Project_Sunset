e7fe4fe7d9c492a30e4d9a6f41adf577ba60badf
LH_cloud <1178437028@qq.com>
2021-05-16 15:11:15 +0800

    微调了一下遮挡，简单设置了一下第一个场景的遮挡

---
+++ Assets/Script/Dynamic_occlusion.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class Dynamic_occlusion : MonoBehaviour
+{
+    private SpriteRenderer spr;
+    private float sprite_width_half;
+    public float adjust_h;
+
+
+    private void Start()
+    {
+        spr = GetComponent<SpriteRenderer>();
+        sprite_width_half = spr.bounds.size.x * 0.5f;
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+      
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, spr.bounds.center.y + adjust_h);
+
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        //Vector3 line = new Vector3(spr.transform.position.x, spr.transform.position.y + adjust_h, spr.transform.position.z);
+        Vector3 line = new Vector3(spr.bounds.center.x, spr.bounds.center.y + adjust_h, spr.bounds.center.z);
+        Gizmos.DrawLine(line + Vector3.left * sprite_width_half, line - Vector3.left * sprite_width_half);
+    }
+}
